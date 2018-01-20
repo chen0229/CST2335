e0c8913a6bfbf40cb5b2806ddc7bf271c507011d
jihon <chen0229@algonquinlive.com>
2018-01-20 16:32:05 -0500

    Added 3 activities for Lab 2

--- app/src/main/java/com/example/jihon/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/jihon/androidlabs/LoginActivity.java
@@ -1,18 +1,14 @@
 package com.example.jihon.androidlabs;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class StartActivity extends Activity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-
-
+        setContentView(R.layout.activity_login);
     }
 }
